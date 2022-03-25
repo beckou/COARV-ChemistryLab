065e700f90b10ceb584be7cfa711617533cf599e
DorianMalguid <dorian.malguid@outlook.fr>
2022-03-25 21:14:12 +0100

    hotfix liquides

--- Assets/Scripts/equation1.cs
+++ Assets/Scripts/equation1.cs
@@ -12,10 +12,16 @@ public class equation1 : MonoBehaviour
     public GameObject flask3;
     public GameObject contenant;
 
+    // Contenu liquide dans le récipient
+    protected Liquid liquidContent;
+    // Volume de liquide minimal requis pour valider l’énigme
+    protected const float minGoodLiquidVolume = 0.1f;
+    protected const float maxBadLiquidVolume = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
 
     }
 
@@ -40,12 +46,14 @@ public class equation1 : MonoBehaviour
         contenant = Instantiate(contenant, table.transform);
         contenant.transform.Translate(new Vector3(0.25f, 1.297f, -0.5f));
         contenant.name = "contenant";
+
+        liquidContent = contenant.GetComponentInChildren<Liquid>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //On cherche le contenant pour vérifier le nombre de gouttes qu'il a reçu de chaque fiole
+        /*//On cherche le contenant pour vérifier le nombre de gouttes qu'il a reçu de chaque fiole
         GameObject contenant = GameObject.Find("contenant");
         compterGouttes compteGouttes = contenant.GetComponent<compterGouttes>();
 
@@ -57,7 +65,34 @@ public class equation1 : MonoBehaviour
         else
         {
             resolue = false;
+        }*/
+
+
+        // On vérifie si les liquides constituant le contenu liquide du récipient sont cohérents avec la condition de validation
+        // Un liquide est caractérisé par sa couleur.
+        bool liquideBleuOk = false;
+        bool liquideRoseOk = false;
+        bool liquideVertOk = true;
+
+        foreach (KeyValuePair<Color, float> lv in liquidContent.ContainedLiquids)
+        {
+            if (lv.Key == flask1.GetComponentInChildren<InfiniteContainer>().liquidColour && lv.Value > minGoodLiquidVolume)
+            {
+                liquideBleuOk = true;
+            }
+
+            if (lv.Key == flask2.GetComponentInChildren<InfiniteContainer>().liquidColour && lv.Value > minGoodLiquidVolume)
+            {
+                liquideRoseOk = true;
+            }
+
+            if (lv.Key == flask3.GetComponentInChildren<InfiniteContainer>().liquidColour && lv.Value > maxBadLiquidVolume)
+            {
+                liquideVertOk = false;
+            }
         }
-        
+
+        resolue = liquideBleuOk && liquideRoseOk && liquideVertOk;
+
     }
 }

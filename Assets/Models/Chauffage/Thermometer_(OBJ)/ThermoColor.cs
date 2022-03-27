d0d2581999f65473ff49a13778db8b5f2020d5d3
feldel7 <felixdelattre007@gmail.com>
2022-03-27 22:48:54 +0200

    variation de couleur
    Le script fonctionne : la cible (Heating Plate à titre d'exemple) change de couleur selon la position du curseur sur le thermomètre.

--- Assets/Models/Chauffage/Thermometer_(OBJ)/ThermoColor.cs
+++ Assets/Models/Chauffage/Thermometer_(OBJ)/ThermoColor.cs
@@ -10,20 +10,22 @@ public class ThermoColor : MonoBehaviour
     public float coloration_rouge;
     public float position_max;
     public float position_min;
+    public string nomCible = "Heating Plate"; //l'objet ou le liquide que l'on souhaite colorer
+
     // Start is called before the first frame update
     void Start()
     {
-        aColorer = GameObject.Find("Heating Plate");
+        aColorer = GameObject.Find(nomCible);
         myRenderer = aColorer.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.transform.position.z);
-        coloration_rouge = (gameObject.transform.position.z - position_min) / (position_max - position_min);// float entre 0 et 1
-        myColor = new Color(coloration_rouge, 0.0f, 0.0f, 0.0f);
-        gameObject.GetComponent<Renderer>().material.color = myColor;
-        myRenderer.material.color = myColor;
+        // lerp the color between min and max position
+        coloration_rouge = (gameObject.transform.position.y - position_min) / (position_max - position_min);// float entre 0 et 1
+        myColor = new Color(0.5f+ coloration_rouge /2, 0.6f, 0.6f, 0.0f);
+        gameObject.GetComponent<Renderer>().material.color = myColor; //on colorie le curseur
+        myRenderer.material.color = myColor; //on colorie la cible
     }
 }
